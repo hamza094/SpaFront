ca3d4afeca23e9f38e4437ed4b41ba876faf656c
hamza094 <hamza_pisces@live.com>
2024-05-12 01:17:32 +0500

    add pagination to index link page

--- pages/links/index.vue
+++ pages/links/index.vue
@@ -1,29 +1,28 @@
 <script setup lang="ts">
+import axios from "axios";
+import { TailwindPagination } from 'laravel-vue-pagination';
+
+const data = ref(null);
+const page = ref(useRoute().query.page || 1);
+
+await getLinks(); 
+
+let links = computed(()=>data.value.data);
+
+watch(page, async()=>{
+  getLinks();
+  useRouter().push({ query: { page:page.value } });
+});
+
+async function getLinks(){
+  const {data:res} = await axios.get(`/links?page=${page.value}`);
+  data.value = res;
+}
 
 definePageMeta({
   middleware:["auth"],
 });
 
-const links = [
-  {
-    short_link: "234jlsfsf",
-    full_link: "https://vueschool.io",
-    views: 3,
-    id: 1,
-  },
-  {
-    short_link: "adfaowerw",
-    full_link: "https://google.com",
-    views: 1,
-    id: 2,
-  },
-  {
-    short_link: "234sfdjaip",
-    full_link: "https://vuejsnation.com/",
-    views: 0,
-    id: 3,
-  },
-];
 </script>
 <template>
   <div>
@@ -84,6 +83,10 @@ const links = [
           </tr>
         </tbody>
       </table>
+       <TailwindPagination
+        :data="data"
+        @pagination-change-page="page= $event"
+    />
       <div class="mt-5 flex justify-center"></div>
     </div>
 
